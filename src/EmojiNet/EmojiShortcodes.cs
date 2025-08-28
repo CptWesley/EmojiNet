7a432e3c6bc69c57e65699a8ff7e842c19a42b59
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-08-28 16:52:13 +0300

    no message

--- src/EmojiNet/EmojiShortcodes.cs
+++ src/EmojiNet/EmojiShortcodes.cs
@@ -5,6 +5,9 @@ namespace EmojiNet;
 /// </summary>
 public sealed class EmojiShortcodes
 {
+    private readonly Lazy<IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>>> byLanguageThenByDatabase;
+    private readonly Lazy<IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>>> byDatabaseThenByLanguage;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="EmojiShortcodes"/> class.
     /// </summary>
@@ -13,28 +16,51 @@ public sealed class EmojiShortcodes
     {
         Emoji = emoji;
 
-        var langs = EmojiShortcodesDatabase.LanguageNames;
-        var dbs = EmojiShortcodesDatabase.DatabaseNames;
-
-        var byLang = new Dictionary<string, Dictionary<string, IReadOnlyList<string>>>();
-        var byDb = new Dictionary<string, Dictionary<string, IReadOnlyList<string>>>();
-
-        foreach (var lang in langs)
+        byLanguageThenByDatabase = new(() =>
         {
-            byLang[lang] = new Dictionary<string, IReadOnlyList<string>>();
-        }
+            var langs = EmojiShortcodesDatabase.LanguageNames;
+            var dbs = EmojiShortcodesDatabase.DatabaseNames;
 
-        foreach (var db in dbs)
+            var byLang = new Dictionary<string, Dictionary<string, IReadOnlyList<string>>>();
+
+            foreach (var lang in langs)
+            {
+                var byDb = new Dictionary<string, IReadOnlyList<string>>();
+                byLang[lang] = byDb;
+
+                foreach (var db in dbs)
+                {
+                    byDb[db] = Array.Empty<string>();
+                }
+            }
+
+            return byLang.AsReadOnly(
+                static x => x.Key,
+                static x => x.Value.AsReadOnly());
+        });
+
+        byDatabaseThenByLanguage = new(() =>
         {
-            byDb[db] = new Dictionary<string, IReadOnlyList<string>>();
-        }
+            var langs = EmojiShortcodesDatabase.LanguageNames;
+            var dbs = EmojiShortcodesDatabase.DatabaseNames;
 
-        ByLanguageThenByDatabase = byLang.AsReadOnly(
-            static x => x.Key,
-            static x => x.Value.AsReadOnly());
-        ByDatabaseThenByLanguage = byDb.AsReadOnly(
-            static x => x.Key,
-            static x => x.Value.AsReadOnly());
+            var byDb = new Dictionary<string, Dictionary<string, IReadOnlyList<string>>>();
+
+            foreach (var db in dbs)
+            {
+                var byLang = new Dictionary<string, IReadOnlyList<string>>();
+                byDb[db] = byLang;
+
+                foreach (var lang in langs)
+                {
+                    byLang[db] = Array.Empty<string>();
+                }
+            }
+
+            return byDb.AsReadOnly(
+                static x => x.Key,
+                static x => x.Value.AsReadOnly());
+        });
     }
 
     /// <summary>
@@ -45,10 +71,10 @@ public sealed class EmojiShortcodes
     /// <summary>
     /// Accesses the shortcodes on a by-language-then-by-database fashion.
     /// </summary>
-    public IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>> ByLanguageThenByDatabase { get; }
+    public IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>> ByLanguageThenByDatabase => byLanguageThenByDatabase.Value;
 
     /// <summary>
     /// Accesses the shortcodes on a by-database-then-by-language fashion.
     /// </summary>
-    public IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>> ByDatabaseThenByLanguage { get; }
+    public IReadOnlyDictionary<string, IReadOnlyDictionary<string, IReadOnlyList<string>>> ByDatabaseThenByLanguage => byDatabaseThenByLanguage.Value;
 }
